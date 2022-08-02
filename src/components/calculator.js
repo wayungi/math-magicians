4b1b4ca86ff48b706e82e47dc3bdbeaac2ee752e
wayungi <fwayungi@gmail.com>
2022-08-02 21:55:53 +0300

    Style calculator display

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -10,9 +10,9 @@ class Calculator extends Component {
           <button type="button" className="numpad">AC</button>
           <button type="button" className="numpad">+/-</button>
           <button type="button" className="numpad">%</button>
-          <button type="button" className="numpad">+</button>
+          <button type="button" className="numpad operator">+</button>
 
-          <button type="button" className="numpad">7</button>
+          <button type="button" className="numpad ">7</button>
           <button type="button" className="numpad">8</button>
           <button type="button" className="numpad">9</button>
           <button type="button" className="numpad">&times;</button>
@@ -20,16 +20,16 @@ class Calculator extends Component {
           <button type="button" className="numpad">4</button>
           <button type="button" className="numpad">5</button>
           <button type="button" className="numpad">6</button>
-          <button type="button" className="numpad">-</button>
+          <button type="button" className="numpad operator">-</button>
 
           <button type="button" className="numpad">1</button>
           <button type="button" className="numpad">2</button>
           <button type="button" className="numpad">3</button>
-          <button type="button" className="numpad">+</button>
+          <button type="button" className="numpad operator">+</button>
 
           <button type="button" className="numpad zero">0</button>
           <button type="button" className="numpad">.</button>
-          <button type="button" className="numpad">=</button>
+          <button type="button" className="numpad operator">=</button>
         </div>
       </div>
     );
