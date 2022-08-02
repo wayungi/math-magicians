93f66c786c03757c687dc8b2ba564741c73cef82
wayungi <fwayungi@gmail.com>
2022-08-02 21:40:01 +0300

    Add number pads

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -5,8 +5,32 @@ class Calculator extends Component {
   render() {
     return (
       <div className="calculator">
-        <div className="screen">0</div>
-        <div className="keyboad">Keyboads</div>
+        <div className="screen ">0</div>
+        <div className="keyboad">
+          <button type="button" className="numpad">AC</button>
+          <button type="button" className="numpad">+/-</button>
+          <button type="button" className="numpad">%</button>
+          <button type="button" className="numpad">+</button>
+
+          <button type="button" className="numpad">7</button>
+          <button type="button" className="numpad">8</button>
+          <button type="button" className="numpad">9</button>
+          <button type="button" className="numpad">&times;</button>
+
+          <button type="button" className="numpad">4</button>
+          <button type="button" className="numpad">5</button>
+          <button type="button" className="numpad">6</button>
+          <button type="button" className="numpad">-</button>
+
+          <button type="button" className="numpad">1</button>
+          <button type="button" className="numpad">2</button>
+          <button type="button" className="numpad">3</button>
+          <button type="button" className="numpad">+</button>
+
+          <button type="button" className="numpad">0</button>
+          <button type="button" className="numpad">.</button>
+          <button type="button" className="numpad">=</button>
+        </div>
       </div>
     );
   }
