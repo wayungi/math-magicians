d2dd188ace3608f48b4c20bb9a6f2cf010c4b547
wayungi <fwayungi@gmail.com>
2022-08-02 22:48:22 +0300

    Convert function to class

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+/* eslint-disable react/prefer-stateless-function */
+
+import { Component } from 'react';
 import Calculator from './components/calculator';
 import './components/calculator.css';
 
-function App() {
-  return (
-    <Calculator />
-  );
+class App extends Component {
+  render() {
+    return <Calculator />;
+  }
 }
 
 export default App;
