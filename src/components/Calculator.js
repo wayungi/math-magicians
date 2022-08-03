9e2421ce120c5c266afd4aeba52d9abcc998768b
wayungi <fwayungi@gmail.com>
2022-08-03 22:25:34 +0300

    Fix linter errors

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,6 +1,8 @@
-/* eslint-disable  */
-
-/* eslint-disable react/prefer-stateless-function */
+/**
+ * /* eslint-disable react/prefer-stateless-function
+ *
+ * @format
+ */
 
 import React from 'react';
 import calculate from '../logic/calculate';
@@ -17,71 +19,70 @@ class Calculator extends React.Component {
   };
 
   render() {
-    const {total, next} = this.state
+    const { total, next } = this.state;
     return (
-      <div className='calculator'>
-        <div className='screen '>{next || total}</div>
-        <div className='keyboad'>
-          <button type='button' className='numpad' onClick={this.clickHandler}>
+      <div className="calculator">
+        <div className="screen ">{next || total}</div>
+        <div className="keyboad">
+          <button type="button" className="numpad" onClick={this.clickHandler}>
             AC
           </button>
-          <button type='button' className='numpad' onClick={this.clickHandler}>
+          <button type="button" className="numpad" onClick={this.clickHandler}>
             +/-
           </button>
-          <button type='button' className='numpad' onClick={this.clickHandler}>
+          <button type="button" className="numpad" onClick={this.clickHandler}>
             %
           </button>
-          <button type='button' className='numpad operator' onClick={this.clickHandler} >
+          <button type="button" className="numpad operator" onClick={this.clickHandler}>
             +
           </button>
 
-          <button type='button' className='numpad' onClick={this.clickHandler}>
+          <button type="button" className="numpad" onClick={this.clickHandler}>
             7
           </button>
-          <button type='button' className='numpad' onClick={this.clickHandler}>
+          <button type="button" className="numpad" onClick={this.clickHandler}>
             8
           </button>
-          <button type='button' className='numpad' onClick={this.clickHandler}>
+          <button type="button" className="numpad" onClick={this.clickHandler}>
             9
           </button>
-          <button type='button' className='numpad operator' onClick={this.clickHandler}>
+          <button type="button" className="numpad operator" onClick={this.clickHandler}>
             x
-            
           </button>
 
-          <button type='button' className='numpad' onClick={this.clickHandler}>
+          <button type="button" className="numpad" onClick={this.clickHandler}>
             4
           </button>
-          <button type='button' className='numpad' onClick={this.clickHandler}>
+          <button type="button" className="numpad" onClick={this.clickHandler}>
             5
           </button>
-          <button type='button' className='numpad' onClick={this.clickHandler}>
+          <button type="button" className="numpad" onClick={this.clickHandler}>
             6
           </button>
-          <button type='button' className='numpad operator' onClick={this.clickHandler}>
+          <button type="button" className="numpad operator" onClick={this.clickHandler}>
             -
           </button>
 
-          <button type='button' className='numpad' onClick={this.clickHandler}>
+          <button type="button" className="numpad" onClick={this.clickHandler}>
             1
           </button>
-          <button type='button' className='numpad' onClick={this.clickHandler}>
+          <button type="button" className="numpad" onClick={this.clickHandler}>
             2
           </button>
-          <button type='button' className='numpad' onClick={this.clickHandler}>
+          <button type="button" className="numpad" onClick={this.clickHandler}>
             3
           </button>
-          <button type='button' className='numpad operator' onClick={this.clickHandler}>
-           ÷
+          <button type="button" className="numpad operator" onClick={this.clickHandler}>
+            ÷
           </button>
 
-          <button type='button' className='numpad zero' onClick={this.clickHandler}>
+          <button type="button" className="numpad zero" onClick={this.clickHandler}>
             0
           </button>
-          <button type='button' className='numpad' onClick={this.clickHandler}>
+          <button type="button" className="numpad" onClick={this.clickHandler}>
             .
           </button>
-          <button type='button' className='numpad operator' onClick={this.clickHandler}>
+          <button type="button" className="numpad operator" onClick={this.clickHandler}>
             =
           </button>
         </div>
