253e03265b1161759fdc811edc0ad33d18232aa1
wayungi <fwayungi@gmail.com>
2022-08-03 19:28:57 +0300

    Remove function component

--- src/App.js
+++ src/App.js
@@ -1,29 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable react/prefer-stateless-function */
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Component } from 'react';
+import Calculator from './components/calculator';
+import './components/calculator.css';
+
+class App extends Component {
+  render() {
+    return <Calculator />;
+  }
 }
 
 export default App;
