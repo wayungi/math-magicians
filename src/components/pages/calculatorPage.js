bf0989383bff38aa120963a18f36a45d2d3b90ad
wayungi <fwayungi@gmail.com>
2022-08-10 11:07:45 +0300

    Add calculator page styles

--- src/components/pages/calculatorPage.js
+++ src/components/pages/calculatorPage.js
@@ -1,9 +1,25 @@
+/** @format */
+
 import Calculator from '../Calculator';
+import styles from './calculatorPage.module.css';
 
 const CalculatorPage = () => (
-  <div>
+  <div className={styles.contentWrapper}>
     <h1>Lets do some math</h1>
-    <Calculator />
+    <div className={styles.content}>
+      <div className={styles.imageWrapper}>
+        <img src="https://picsum.photos/500/240" alt="random images" />
+      </div>
+      <div className={styles.calculatorWrapper}>
+        <Calculator />
+      </div>
+    </div>
+    <div className={styles.credits}>
+      <p>
+        Pictures used on this page are dynamically generated from
+        <a href="https://picsum.photos/"> Lorem Picsum</a>
+      </p>
+    </div>
   </div>
 );
 
